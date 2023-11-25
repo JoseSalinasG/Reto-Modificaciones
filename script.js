c1a8632466cf0f3ffe9be96db3c1dbabe8e69e36
JoseSalinasG <149660068+JoseSalinasG@users.noreply.github.com>
2023-11-25 16:31:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const circulo = document.getElementById('circulo');
+circulo.style.backgroundColor = 'red';
+circulo.style.fontFamily = 'roboto';
+circulo.style.border = '4px solid white';
+circulo.style.fontSize = '30px'
+
+const fondo = document.getElementsByTagName('body')[0];
+fondo.style.backgroundColor = 'gray';
