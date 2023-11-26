8e722c5422312a5ef2683d2c8ac80f66b1833ba2
JoseSalinasG <149660068+JoseSalinasG@users.noreply.github.com>
2023-11-26 16:36:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ const circulo = document.getElementById('circulo');
 const texto = document.getElementsByClassName('texto')[0];
 const boton = document.getElementsByClassName('boton')[0];
 
-//Cambio de color del id Circulo.
-circulo.classList.toggle('naranja');
-circulo.classList.toggle('rojo');
-circulo.classList.toggle('verde');
-
 // Modificacion directa de css 
 texto.style.fontSize = '30px';
 texto.classList.toggle = 'negrita';
 // modificado por el Set de atributos.
 boton.setAttribute('style','font-size: 25px ; border-radius: 20%; background-color : gray;');
+
+// Agregar un event listener al botón
+boton.addEventListener('click', cambiarColorCirculo);
+
+// Colores permitidos
+const coloresPermitidos = ['#FF0000', '#00FF00', '#0000FF'];
+
+// Función para cambiar el color del círculo
+function cambiarColorCirculo() {
+    // Seleccionar un color al azar de la lista de colores permitidos
+    const nuevoColor = coloresPermitidos[Math.floor(Math.random() * coloresPermitidos.length)];
+  
+    // Aplicar el nuevo color al círculo
+    circulo.style.backgroundColor = nuevoColor;
+}
+
+
